a5734006c0e144f7810010b79a838521cf1a6382
Mrpsousa <marcos.sousa@lactec.org.br>
2021-04-07 16:36:36 -0300

    feature: email com sendgrid

--- project/subscriptions/views.py
+++ project/subscriptions/views.py
@@ -15,8 +15,8 @@ def subscribe(request):
                                     form.cleaned_data)
 
             mail.send_mail('Confirmação de inscrição', body,
-                           'contato@eventex.com.br',
-                           ['contato@eventex.com.br', form.cleaned_data['email']])
+                           'mrpsousa@outlook.com',
+                           ['mrpsousa@outlook.com', form.cleaned_data['email']])
             
             messages.success(request, 'Inscrição realizada com sucesso!')
             return HttpResponseRedirect('/inscricao/')
