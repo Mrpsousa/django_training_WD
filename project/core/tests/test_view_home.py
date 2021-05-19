044c69efa5deac1bd612b967c7bac432f3a705f4
Mrpsousa <urameshi.uba@gmail.com>
2021-05-19 15:53:18 -0300

    teste

--- project/core/tests/test_view_home.py
+++ project/core/tests/test_view_home.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 
+
 class HomeTest(TestCase):
     def setUp(self):
         self.response = self.client.get('/')
@@ -7,7 +8,6 @@ class HomeTest(TestCase):
     def test_get(self):
         """ GET / must return status code 200"""
         self.assertEqual(200, self.response.status_code)
-    
 
     def test_template(self):
         """  Must use index.html"""
